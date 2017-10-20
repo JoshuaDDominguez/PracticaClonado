752e0af35ee7bdbc255a834f0fbcd517830c00ef
mfrancomartinez <mfrancomartinez@victoria27.danielcastelao.org>
2017-10-20 09:01:49 +0200

    Practica de clonado 2 veces

--- src/practicaclonado/PracticaClonado.java
+++ src/practicaclonado/PracticaClonado.java
@@ -5,6 +5,9 @@ public class PracticaClonado {
 
     public static void main(String[] args) {
         System.out.println("Practica clonado");
+        
+        //Marcos
+        System.out.println("Practica de clonado otra vez");
     }
     
 }
